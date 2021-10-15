5e3e2caf70119bc0bdd836bb0ef49733b2541209
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-10-15 00:58:57 -0600

    Give global resources a better name and structure

--- app/stencil.config.ts
+++ app/stencil.config.ts
@@ -3,8 +3,8 @@ import { Config } from '@stencil/core';
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
-  globalStyle: 'src/global/app.css',
-  globalScript: 'src/global/app.ts',
+  globalStyle: 'src/main.css',
+  globalScript: 'src/main.ts',
   taskQueue: 'async',
   outputTargets: [
     {
