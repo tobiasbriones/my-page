723d68221c5c449b1081c49a05111cb1e2feafda
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-10-15 18:29:49 -0600

    Add copyright license header

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2021 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of Brief And Colorful Personal Landing Page.
+ *
+ * This source code is licensed under the MIT License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/MIT.
+ */
+
 import { Component, h } from '@stencil/core';
 
 @Component({
