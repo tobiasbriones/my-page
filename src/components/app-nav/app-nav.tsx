c5d27e0efbe35bbcfd47431b727eeb613321aab5
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-10-26 17:05:31 -0600

    Integrate component app-nav-item into app-nav

--- src/components/app-nav/app-nav.tsx
+++ src/components/app-nav/app-nav.tsx
@@ -11,7 +11,7 @@
  */
 
 import { Component, Event, EventEmitter, h, Prop } from '@stencil/core';
-import { NavItem } from '../app-home/app-user-header/app-user-nav/user-nav';
+import { NavItem } from './nav-item';
 
 @Component({
   tag: 'app-nav',
@@ -33,19 +33,13 @@ export class AppNav {
   }
 
   render() {
-    const getClass = (item: NavItem) => item.id === this.selectedItem?.id ? 'selected' : '';
     const mapItem = (item: NavItem) => (
-      <li
-        id={ item.id.toString() }
-        key={ item.id.toString() }
-        class={ getClass(item) }
-        onClick={ e => this.onItemClick(e) }
-      >
-        <img
-          src={ this.getIconSrc(item) }
-          alt={ item.value }
+      <li key={ item.id.toString() }>
+        <app-nav-item
+          item={ item }
+          isSelected={ item.id === this.selectedItem?.id }
+          onItemClick={ e => this.onItemClick(e) }
         />
-        <span>{ item.value }</span>
       </li>
     );
     return (
@@ -58,16 +52,8 @@ export class AppNav {
     );
   }
 
-  onItemClick(event: MouseEvent) {
-    const el = event.currentTarget as HTMLUListElement;
-    const id = parseInt(el.id);
-    const item = this.items.find(item => item.id === id);
+  onItemClick(event: CustomEvent<NavItem>) {
+    const item = event.detail;
     this.itemClick?.emit(item);
-  }
-
-  private getIconSrc(item: NavItem): string {
-    return this.selectedItem?.id === item.id ?
-      `${ item.iconSrc }-active.svg` :
-      `${ item.iconSrc }.svg`;
   }
 }
