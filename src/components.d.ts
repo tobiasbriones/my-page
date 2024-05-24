6fa541681a0e9b981bcc4323ead34b295f13dd52
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-05-24 16:16:51 -0600

    Update components.d.ts
    It's generated when building.

--- src/components.d.ts
+++ src/components.d.ts
@@ -1,15 +1,3 @@
-/*
- * Copyright (c) 2021 Tobias Briones. All rights reserved.
- *
- * SPDX-License-Identifier: MIT
- *
- * This file is part of My Page.
- *
- * This source code is licensed under the MIT License found in the LICENSE file
- * in the root directory of this source tree or at
- * https://opensource.org/licenses/MIT.
- */
-
 /* eslint-disable */
 /* tslint:disable */
 /**
