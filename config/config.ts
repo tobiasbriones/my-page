3081c11165350a4d36789b573075aba0ab8fcee3
Tobias Briones <tbriones@mathswe.com>
2024-08-08 12:35:53 -0600

    Update copyright license header at ./config with compact version

--- config/config.ts
+++ config/config.ts
@@ -1,14 +1,6 @@
-/*
- * Copyright (c) 2021 Tobias Briones. All rights reserved.
- *
- * SPDX-License-Identifier: MIT
- *
- * This file is part of My Page.
- *
- * This source code is licensed under the MIT License found in the LICENSE file
- * in the root directory of this source tree or at
- * https://opensource.org/licenses/MIT.
- */
+// Copyright (c) 2021 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: MIT
+// This file is part of https://github.com/tobiasbriones/my-page.
 
 import { prodConfig } from "./prod.config";
 import { devConfig } from "./dev.config";
